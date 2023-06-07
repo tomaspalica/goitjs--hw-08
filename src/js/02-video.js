794fd7c12d44e852bd52f37fc70da809f4c19419
tomaspalica <tomasss.palica@gmail.com>
2023-06-07 13:14:57 +0200

    finished task 2 and 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle')
+const videoPlayer = document.querySelector("#vimeo-player")
+const framePlayer = new Player(videoPlayer)
 
+const onPlay = () => framePlayer.getCurrentTime().then(function(seconds) {
+    localStorage.setItem("videoplayer-current-time", seconds)
+    console.log(seconds)
+}).catch(function(error) {
+   
+});
+const savedSetting =localStorage.getItem("videoplayer-current-time")
+const parsedSetting = JSON.parse(savedSetting)
+
+framePlayer.on("timeupdate", throttle(onPlay,1000))
+
+framePlayer.setCurrentTime(parsedSetting)
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+const throttle = require('lodash.throttle')
+const form = document.querySelector(".feedback-form")
+const email = document.querySelector("form.feedback-form input[name='email']")
+const message = document.querySelector("form.feedback-form textarea[name='message']")
 
+const addToStorage = (event) =>{
+    const feedbackFormState = {
+        email : email.value,
+        message : message.value,
+    }
+    localStorage.setItem("feedback-form-state" ,JSON.stringify(feedbackFormState))
+}
+
+email.addEventListener("input", throttle(addToStorage, 500));
+message.addEventListener("input", throttle(addToStorage,500));
+
+const formSend = (event) => {
+    event.preventDefault()
+    console.log(JSON.parse(localStorage.getItem("feedback-form-state")))
+email.value = "";
+message.value = "";
+    localStorage.removeItem("feedback-form-state");
+}
+
+form.addEventListener("submit",formSend)
+const checkStorage = () => {
+    if (localStorage.getItem("feedback-form-state") === ""){
+        return 
+    } else {
+        email.value = JSON.parse(localStorage.getItem("feedback-form-state")).email
+        message.value = JSON.parse(localStorage.getItem("feedback-form-state")).message
+    }
+}
+checkStorage();
